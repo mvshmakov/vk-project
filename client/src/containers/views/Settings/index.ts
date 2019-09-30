025748b4aef18294044e1e9847b6cec1acd91d0c
Egor <shir-egor@yandex.ru>
2019-09-30 12:19:34 +0300

    some refactoring done

--- client/src/containers/views/Settings/index.ts
+++
@@ -1,6 +0,0 @@
-import { connect } from "react-redux";
-import SettingsView, { IStateProps } from "@/components/views/Settings";
-
-const mapStateToProps = ({ account }): IStateProps => ({ user: account.user });
-
-export default connect(mapStateToProps)(SettingsView);
